5394bd49b338cf7e5270aa6d4ad94d14b134d41a
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2025-09-24 18:29:16 +0000

    Vou precisar que você mude o pixel para esse novo aqui.
    <script>
      wi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   `;
 
   const pixelScript = `
-    window.pixelId = "68c8a71af3ef779533afca6a";
+    window.pixelId = "68d4384396d6eb6fca9c6acc";
     var a = document.createElement("script");
     a.setAttribute("async", "");
     a.setAttribute("defer", "");
