30ed0612922ddc9553d91a07dc3bc1b3f24fcddf
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2025-10-01 19:40:35 +0000

    Prepare meu projeto Next.js para ser exportado como um site estático e e

--- next.config.ts
+++ next.config.ts
@@ -11,38 +11,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https' ,
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
